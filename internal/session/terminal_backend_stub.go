23adb6d9ae6c9617ff1fdb3d62485c121fb677ca
Adib Hanna <adibhanna@gmail.com>
2026-03-21 22:43:16 -0500

    Fix gofmt formatting in terminal backend stub

--- internal/session/terminal_backend_stub.go
+++ internal/session/terminal_backend_stub.go
@@ -10,8 +10,12 @@ func NewTerminalBackend(rows, cols uint16) TerminalBackend {
 	return &stubTerminal{}
 }
 
-func (s *stubTerminal) Consume(data []byte)          {}
+func (s *stubTerminal) Consume(data []byte) {}
+
 func (s *stubTerminal) Resize(rows, cols uint16) error { return nil }
-func (s *stubTerminal) Snapshot() []byte               { return nil }
-func (s *stubTerminal) Preview() []byte                { return nil }
-func (s *stubTerminal) Close() error                   { return nil }
+
+func (s *stubTerminal) Snapshot() []byte { return nil }
+
+func (s *stubTerminal) Preview() []byte { return nil }
+
+func (s *stubTerminal) Close() error { return nil }
